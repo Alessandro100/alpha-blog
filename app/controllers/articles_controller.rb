9ea8af50ffc84988d81369e2f9f82da103dfc56d
Alessandro <Kalessandro14@gmail.com>
2016-12-27 00:49:57 +0000

    added destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,13 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was no gud"
+    redirect_to articles_path
+  end
+  
   def new
     @article = Article.new
   end
