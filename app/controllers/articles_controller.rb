56617ebbf1f13180eb7b6ca0d455c0fc0d317345
Alessandro <Kalessandro14@gmail.com>
2016-12-27 01:04:11 +0000

    final product + optimized code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
   
   def index
     @articles = Article.all
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was no gud"
     redirect_to articles_path
@@ -27,15 +27,12 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was gud x2"
       redirect_to article_path(@article)
@@ -47,9 +44,12 @@ class ArticlesController < ApplicationController
   
   
   private
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
+    def set_article
+      @article = Article.find(params[:id])
+    end
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
   
   
 end
